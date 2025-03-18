d5ea6395d024fbf42847ab7cf0c86aedf54c76af
suihanzhou <1327657824@qq.com>
2025-03-18 21:32:18 +0800

    add

---
+++ flip1.js
@@ -0,0 +1,7 @@
+
+
+window.onload = function () {
+    // 从 localStorage 中获取名字
+    var name = localStorage.getItem("userName");
+
+ document.getElementById("response").innerText = "Welcome to Let's Flip, " + response + "!";
